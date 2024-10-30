365c95d29268ccbd6ef44403fec786c4925303b1
Sergey Grigorenko <miraigajettolab@gmail.com>
2024-10-30 19:46:36 +0400

    Gen first corner

--- src/BoxGenerator/BoxGenerator.tsx
+++ src/BoxGenerator/BoxGenerator.tsx
@@ -31,6 +31,20 @@ const getRandomAngleExcluding = (excludedAngles: number[] = [], bufferAngle: num
 
 const getRandomLength = (min: number, max: number) => Math.floor(min + Math.random() * (max - min));
 
+const getRandomPointInTriangle = (p1: Point, p2: Point, p3: Point): Point => {
+  const u = Math.random();
+  const v = Math.random();
+
+  const w = u + v;
+  const adjustedU = w > 1 ? 1 - u : u;
+  const adjustedV = w > 1 ? 1 - v : v;
+
+  return {
+    x: (1 - adjustedU - adjustedV) * p1.x + adjustedU * p2.x + adjustedV * p3.x,
+    y: (1 - adjustedU - adjustedV) * p1.y + adjustedU * p2.y + adjustedV * p3.y,
+  };
+};
+
 const BoxGenerator: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
@@ -42,7 +56,7 @@ const BoxGenerator: React.FC = () => {
 
     context.clearRect(0, 0, context.canvas.width, context.canvas.height);
 
-    const centerPoint = {
+    const centerPoint: Point = {
       x: context.canvas.width / 2,
       y: context.canvas.height / 2,
     };
@@ -72,9 +86,9 @@ const BoxGenerator: React.FC = () => {
     const angle1 = getRandomAngleExcluding();
     const angle2 = getRandomAngleExcluding([angle1]);
     const angle3 = getRandomAngleExcluding([angle1, angle2]);
-    const length1 = getRandomLength(minDimention / 40, minDimention / 4);
-    const length2 = getRandomLength(minDimention / 40, minDimention / 4);
-    const length3 = getRandomLength(minDimention / 40, minDimention / 4);
+    const length1 = getRandomLength(minDimention / 10, minDimention / 4);
+    const length2 = getRandomLength(minDimention / 10, minDimention / 4);
+    const length3 = getRandomLength(minDimention / 10, minDimention / 4);
 
     drawCenterLine(angle1, length1);
     drawCenterLine(angle2, length2);
@@ -82,9 +96,11 @@ const BoxGenerator: React.FC = () => {
 
     const end1 = getEndPoint(centerPoint, angle1, length1);
     const end2 = getEndPoint(centerPoint, angle2, length2);
-    const corner1 = getEndPoint(end1, angle2, length2);
-    drawLine(end1, corner1);
-    drawLine(end2, corner1);
+    const intersection12 = getEndPoint(end1, angle2, length2);
+    const corner12 = getRandomPointInTriangle(end1, end2, intersection12);
+
+    drawLine(end1, corner12);
+    drawLine(end2, corner12);
   }, [canvasRef]);
 
   return (
