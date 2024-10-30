b970f0c4f75a43fc71dda86546b4a54fb6a6e35a
Sergey Grigorenko <miraigajettolab@gmail.com>
2024-10-30 17:33:59 +0400

    Implement getRandomAngleExcluding

--- src/BoxGenerator/BoxGenerator.tsx
+++ src/BoxGenerator/BoxGenerator.tsx
@@ -1,6 +1,25 @@
 import React from "react";
 import { BoxGeneratorInner } from "./inner";
 
+const getRandomAngleExcluding = (excludedAngles: number[], exclutionAngle: number = Math.PI / 2): number => {
+  const getRandomAngle = () => Math.random() * 2 * Math.PI;
+  const angleInExclutionZone = (angle: number): boolean =>
+    excludedAngles.some((excludedAngle) => {
+      const moreThanLower = angle >= excludedAngle - exclutionAngle;
+      const lessThanUpper = angle <= excludedAngle + exclutionAngle;
+      return moreThanLower && lessThanUpper;
+    });
+
+  // Rolling like this until we get a correct answer is kinda bad, but I don't care
+  // This can just loop forever too, if you mess up, but should be fine to get 3 angles like I need
+  let randomAngle = getRandomAngle();
+  while (angleInExclutionZone(randomAngle)) {
+    randomAngle = getRandomAngle();
+  }
+
+  return randomAngle;
+};
+
 const BoxGenerator: React.FC = () => {
   return (
     <BoxGeneratorInner>
