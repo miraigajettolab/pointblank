4d78c5f0ee9083f640d3d3521ad69040d15f1f75
Sergey Grigorenko <miraigajettolab@gmail.com>
2024-10-31 17:31:00 +0400

    Add controls and states

--- src/BoxGenerator/BoxGenerator.tsx
+++ src/BoxGenerator/BoxGenerator.tsx
@@ -1,12 +1,24 @@
 import React, { useEffect, useRef, useState } from "react";
-import { BoxCanvas, BoxGeneratorInner } from "./inner";
+import { BoxCanvas, BoxControls, BoxGeneratorInner, ControlButton } from "./inner";
 import { BoxData, Point, generateBoxData } from "./util";
+
+enum States {
+  PlaceThridVertex,
+  PlaceBackVertex,
+  CheckResults,
+}
 
 const BoxGenerator: React.FC = () => {
   const canvasHeight = window.innerHeight;
   const canvasWidth = window.innerWidth;
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [box, setBox] = useState<BoxData>(generateBoxData(canvasWidth, canvasHeight));
+  const [state, setState] = useState(States.PlaceThridVertex);
+
+  const handleReset = () => {
+    setBox(generateBoxData(canvasWidth, canvasHeight));
+    setState(States.PlaceThridVertex);
+  };
 
   useEffect(() => {
     const context = canvasRef.current?.getContext("2d");
@@ -35,32 +47,44 @@ const BoxGenerator: React.FC = () => {
     drawLine(box.yEnd1, box.corner13);
     drawLine(box.yEnd3, box.corner13);
 
-    drawLine(box.yEnd2, box.corner23);
-    drawLine(box.yEnd3, box.corner23);
+    if (state === States.CheckResults) {
+      drawLine(box.yEnd2, box.corner23);
+      drawLine(box.yEnd3, box.corner23);
 
-    drawLine(box.corner13, box.backCorner);
-    drawLine(box.corner12, box.backCorner);
-    drawLine(box.corner23, box.backCorner);
+      drawLine(box.corner13, box.backCorner);
+      drawLine(box.corner12, box.backCorner);
+      drawLine(box.corner23, box.backCorner);
 
-    drawLine(box.yEnd1, box.vp1, "red", 1);
-    drawLine(box.corner12, box.vp1, "red", 1);
-    drawLine(box.corner13, box.vp1, "red", 1);
-    drawLine(box.backCorner, box.vp1, "red", 1);
+      drawLine(box.yEnd1, box.vp1, "red", 1);
+      drawLine(box.corner12, box.vp1, "red", 1);
+      drawLine(box.corner13, box.vp1, "red", 1);
+      drawLine(box.backCorner, box.vp1, "red", 1);
 
-    drawLine(box.corner12, box.vp2, "green", 1);
-    drawLine(box.yEnd2, box.vp2, "green", 1);
-    drawLine(box.corner23, box.vp2, "green", 1);
-    drawLine(box.backCorner, box.vp2, "green", 1);
+      drawLine(box.corner12, box.vp2, "green", 1);
+      drawLine(box.yEnd2, box.vp2, "green", 1);
+      drawLine(box.corner23, box.vp2, "green", 1);
+      drawLine(box.backCorner, box.vp2, "green", 1);
 
-    drawLine(box.corner13, box.vp3, "blue", 1);
-    drawLine(box.yEnd3, box.vp3, "blue", 1);
-    drawLine(box.corner23, box.vp3, "blue", 1);
-    drawLine(box.backCorner, box.vp3, "blue", 1);
-  }, [canvasRef, box]);
+      drawLine(box.corner13, box.vp3, "blue", 1);
+      drawLine(box.yEnd3, box.vp3, "blue", 1);
+      drawLine(box.corner23, box.vp3, "blue", 1);
+      drawLine(box.backCorner, box.vp3, "blue", 1);
+    }
+  }, [canvasRef, box, state]);
 
   return (
     <BoxGeneratorInner>
       <BoxCanvas ref={canvasRef} width={canvasWidth} height={canvasHeight} />
+      <BoxControls>
+        <ControlButton disabled={state === States.CheckResults} onClick={() => setState(States.PlaceThridVertex)}>
+          Place Front
+        </ControlButton>
+        <ControlButton disabled={state === States.CheckResults} onClick={() => setState(States.PlaceBackVertex)}>
+          Place Back
+        </ControlButton>
+        <ControlButton onClick={() => setState(States.CheckResults)}>Check</ControlButton>
+        <ControlButton onClick={handleReset}>Reset</ControlButton>
+      </BoxControls>
     </BoxGeneratorInner>
   );
 };
--- src/BoxGenerator/inner.ts
+++ src/BoxGenerator/inner.ts
@@ -3,3 +3,30 @@ import styled from "styled-components";
 export const BoxGeneratorInner = styled.div``;
 
 export const BoxCanvas = styled.canvas``;
+
+export const BoxControls = styled.div`
+  position: fixed;
+  display: flex;
+  column-gap: 10px;
+  bottom: 40px;
+  right: 50%;
+  transform: translateX(50%);
+`;
+
+export const ControlButton = styled.button`
+  height: 40px;
+  padding: 0 20px;
+  border: 1px solid black;
+  border-radius: 10px;
+  background-color: white;
+  cursor: pointer;
+
+  &:hover:not(:disabled) {
+    background-color: ivory;
+  }
+
+  &:disabled {
+    border-color: gray;
+    cursor: default;
+  }
+`;
